fbd8f8870b2b8696e3d59049198e74ad90721a6e
Juaki97 <garcia.jjimenez@gmail.com>
2017-05-21 21:44:53 +0200

    - Añadido entorno gráfico

---
+++ src/EnciarCorreo.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by garci on 21/05/2017.
+ */
+public class EnciarCorreo {
+    private JTextField txtDestino;
+    private JTextArea txtMensaje;
+    private JButton btnEnviar;
+    private JTextField txtAsunto;
+}
